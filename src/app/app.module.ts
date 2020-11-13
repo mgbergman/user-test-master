297f7fbd1d8048e75db3d960f94047991dd999fc
Marc Bergman <mgbergman@gmail.com>
2020-11-13 10:10:09 -0500

    Nov 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { UserListComponent } from './user/user-list/user-list.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { VendorListComponent } from './vendor/vendor-list/vendor-list.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
+import { VendorEditComponent } from './vendor/vendor-edit/vendor-edit.component';
+import { BoolDisplayPipe } from './pipes/bool-display.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { UserCreateComponent } from './user/user-create/user-create.component';
     UserListComponent,
     UserEditComponent,
     VendorListComponent,
-    UserCreateComponent
+    UserCreateComponent,
+    VendorEditComponent,
+    BoolDisplayPipe
   ],
   imports: [
     BrowserModule,
